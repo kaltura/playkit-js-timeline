7b6f3ed10e8a6aa7ad172017d45defadf4f9a43f
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2023-06-29 13:56:14 +0300

    add timeline markers to timeline manager

---
+++ flow-typed/types/cue-point-option.ts
@@ -0,0 +1,68 @@
+export type CuePointOptionsObject = {
+  marker?: MarkerOptionsObject,
+  preview?: PreviewOptionsObject
+};
+
+export type TimedCuePointOptionsObject = CuePointOptionsObject & {
+  time: number,
+  presets?: Array<string>
+};
+
+export type KalturaCuePointOptionsObject = {
+  startTime: number,
+  type: string,
+  cuePointId: string,
+  title?: string,
+  quizQuestionData?: QuizQuestionData
+};
+
+export type QuizQuestionData = {
+  onClick: () => void,
+  isMarkerDisabled: () => boolean,
+  index: number,
+  type: number
+};
+
+export type MarkerOptionsObject = {
+  get?: Function | string,
+  props?: Object,
+  color?: string,
+  width?: number,
+  className?: string
+};
+
+export type PreviewOptionsObject = {
+  get?: Function | string,
+  props?: Object,
+  width?: number,
+  height?: number,
+  className?: string,
+  hideTime?: boolean,
+  sticky?: boolean
+};
+
+export type TimelineMarkerDataObject = MarkerOptionsObject & {
+  cuePoints: Array<string>,
+  id?: string,
+  timelinePreviewRef: any,
+  timelineMarkerRef: any,
+  cuePointsData: Array<CuePointMarker>,
+  chaptersData: any,
+  useQuizQuestionMarkerSize: boolean,
+  onMarkerClick: any,
+  isMarkerDisabled: any
+};
+
+export type CuePointMarker = {
+  id: string,
+  type: string,
+  title: string,
+  quizQuestionData: any
+};
+
+export type Chapter = {
+  id: string,
+  title: string,
+  startTime: number,
+  endTime: number
+};
