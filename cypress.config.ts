405ca58e8b7985a89222166c98769a550e1deada
Serhii <serhii.marchenko@Kaltura.com>
2023-09-19 13:25:21 +0300

    fix(FEC-13017): dual-screen preview e2e tests (cypress) (#23)
    * feat(FEC-13017): add dua-screen thumbnails on the timeline
    
    * fix(FEC-13017): dual-preview e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'cypress';
 
 export default defineConfig({
   fileServerFolder: 'cypress/public',
+  defaultCommandTimeout: 30000,
   e2e: {
     supportFile: false,
     watchForFileChanges: false
